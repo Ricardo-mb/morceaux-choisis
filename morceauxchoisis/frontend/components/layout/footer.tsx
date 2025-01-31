65e133873090fdf2b164324964b81bafb3b7333c
KASABITI <aggieksbt@gmail.com>
2025-01-31 10:17:41 +0100

    Project clean up

--- morceauxchoisis/frontend/components/layout/footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="mt-16 bg-secondary py-8 text-secondary-foreground">
-      <div className="container mx-auto px-4 text-center">
-        <p>&copy; 2024 Your Company. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-}
--- morceauxchoisis/frontend/components/providers/apollo-providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { ApolloProvider as Provider } from '@apollo/client';
-import { client } from '@/lib/apollo-client';
-
-export function ApolloWrapper({ children }: { children: React.ReactNode }) {
-  return <Provider client={client}>{children}</Provider>;
-}
