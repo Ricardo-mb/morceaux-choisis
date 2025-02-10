872ebf5cc56115adf50f2bcfd99a701fa7f5f4b8
KASABITI <aggieksbt@gmail.com>
2025-02-10 20:48:52 +0100

    empty commit

--- morceauxchoisis/frontend/next.config.ts
+++ morceauxchoisis/frontend/next.config.ts
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 }
 
 module.exports = nextConfig
