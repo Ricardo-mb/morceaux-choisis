c9f87ff6ed0b51faf13a40c96b8e805115eef78b
KASABITI <aggieksbt@gmail.com>
2025-03-02 00:34:08 +0100

    empty commit

---
+++ morceaux-choisis-main/ricardomboukou/backend/data/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    id: "1",
+    name: "Project 1",
+    description: "Project 1 description",
+    imageUrl: "https://via.placeholder.com/150",
+    projectUrl: "https://todo-graph-36fa150ad0fe.herokuapp.com/",
+    status: "In Progress",
+  },
+  {
+    id: "2",
+    name: "Project 2",
+    description: "Project 2 description",
+    imageUrl: "https://via.placeholder.com/150",
+    projectUrl: "https://mon-portfolio-creatif-n9zrh22.gamma.site/",
+    status: "In Progress",
+  },
+  {
+    id: "3",
+    name: "Project 3",
+    description: "Project 3 description",
+    imageUrl: "https://via.placeholder.com/150",
+    projectUrl: "https://mon-portfolio-creatif-n9zrh22.gamma.site/",
+    status: "In Progress",
+  },
+];
+
+export { projects };
